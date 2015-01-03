271373eeab7e3c04b4e2f259e9f1ef6aba036c65
Tian Lan <tianlan2007@gmail.com>
2015-01-03 11:47:49 -0500

    change control

--- implicit/R/implicit.R
+++ implicit/R/implicit.R
@@ -1,14 +1,12 @@
 source('R/RcppExports.R')
 
-
+############################################
+##TODO:Give warning if not converged?     ##
+############################################
 #Set the control according to user input
-implicit.control <- function(epsilon = 1e-08, maxit = 10000, trace = FALSE) 
+implicit.control <- function(trace = FALSE) 
 {
-  if (!is.numeric(epsilon) || epsilon <= 0) 
-    stop("value of 'epsilon' must be > 0")
-  if (!is.numeric(maxit) || maxit <= 0) 
-    stop("maximum number of iterations must be > 0")
-  list(epsilon = epsilon, maxit = maxit, trace = trace)
+  list(trace = trace)
 }
 
 # A generic function to dispatch calls
