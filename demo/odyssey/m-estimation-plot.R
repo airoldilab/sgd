32ee0cd201b5a41e2a47faf275bd0ed291b296b8
Dustin Tran <dustinviettran@gmail.com>
2015-09-13 20:48:44 -0400

    add rmsprop to m-estimation plot

--- demo/odyssey/m-estimation-plot.R
+++ demo/odyssey/m-estimation-plot.R
@@ -30,31 +30,38 @@ dat <- data.frame(y=data$y, x=data$X)
 sgd.theta1 <- sgd(y ~ .-1, data=dat, model="m",
   sgd.control=list(
   method="sgd",
-  lr.control=c(15, NA, NA, 1/2), npass=10, pass=T, size=0.5*N, start=rep(5,d)))
+  lr.control=c(15, NA, NA, 1/2), npass=30, pass=T, size=0.5*N, start=rep(5,d)))
 sgd.theta2 <- sgd(y ~ .-1, data=dat, model="m",
   sgd.control=list(
   method="ai-sgd",
-  lr.control=c(15, NA, NA, 2/3), npass=10, pass=T, size=0.5*N, start=rep(5,d)))
+  lr.control=c(15, NA, NA, 2/3), npass=30, pass=T, size=0.5*N, start=rep(5,d)))
+sgd.theta3 <- sgd(y ~ .-1, data=dat, model="m",
+  sgd.control=list(
+  method="ai-sgd",
+  lr="rmsprop",
+  lr.control=c(0.01, NA, NA), npass=30, pass=T, size=0.5*N, start=rep(5,d)))
+sgd.theta4 <- sgd(y ~ .-1, data=dat, model="m",
+  sgd.control=list(
+  method="sgd",
+  lr="rmsprop",
+  lr.control=c(0.01, NA, NA), npass=30, pass=T, size=0.5*N, start=rep(5,d)))
 
-sgd.list <- list("ai-sgd"=sgd.theta1, "sgd"=sgd.theta2)
+sgd.list <- list("ai-sgd"=sgd.theta1, "sgd"=sgd.theta2, "sgd+rmsprop"=sgd.theta4,"ai-sgd+rmsprop"=sgd.theta3)
 
 p1 <- plot(sgd.list, data$theta, type="mse-param") +
   geom_hline(yintercept=1.5, color="green") +
   ggplot2::scale_y_continuous(
-    breaks=seq(1, 10, 1))
+    breaks=seq(1, 10, 1),
+    limits=c(1, 10))
+      #legend.position=c(0.55, 0.6),
 p2 <- plot(sgd.list, data$theta, type="mse-param", xaxis="runtime") +
   geom_hline(yintercept=1.5, color="green") +
   ggplot2::scale_y_continuous(
-    breaks=seq(1, 10, 1))
+    breaks=seq(1, 10, 1),
+    limits=c(1, 10))
 pdf("temp/huber_1.pdf", width=4, height=4)
 print(p1)
 dev.off()
 pdf("temp/huber_2.pdf", width=4, height=4)
 print(p2)
 dev.off()
-
-#library(hqreg)
-#time_start <- proc.time()[3]
-#hqreg <- hqreg(data$X, as.vector(data$y), method = "huber",
-#  gamma=3, alpha=1, lambda=c(1, 0))
-#times <- as.numeric(proc.time()[3] - time_start)
