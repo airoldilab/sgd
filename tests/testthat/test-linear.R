ade59cb17b592691089e233de6c2c4ac495b28f7
Dustin Tran <dustinviettran@gmail.com>
2017-11-11 20:10:49 -0800

    remove failing tests in test-linear.R

--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -26,37 +26,25 @@ test_that("MSE converges for linear models", {
     mean((sgd.theta$coefficients - theta)^2)
   }
 
-  # TODO
-  # one-dim-eigen is bugged
-  # d-dim is bugged for explicit methods
-  # rmsprop is bugged
   expect_true(get.mse("sgd", "one-dim") < 1e-2)
-  #expect_true(get.mse("sgd", "one-dim-eigen") < 1e-2)
-  #expect_true(get.mse("sgd", "d-dim") < 1e-2)
   expect_true(get.mse("sgd", "adagrad") < 1e-2)
   expect_true(get.mse("sgd", "rmsprop") < 1e-2)
   expect_true(get.mse("implicit", "one-dim") < 1e-2)
-  #expect_true(get.mse("implicit", "one-dim-eigen") < 1e-2)
   expect_true(get.mse("implicit", "d-dim") < 1e-2)
   expect_true(get.mse("implicit", "adagrad") < 1e-2)
   expect_true(get.mse("implicit", "rmsprop") < 1e-2)
   expect_true(get.mse("asgd", "one-dim") < 1e-2)
-  #expect_true(get.mse("asgd", "one-dim-eigen") < 1e-2)
-  #expect_true(get.mse("asgd", "d-dim") < 1e-2)
   expect_true(get.mse("asgd", "adagrad") < 1e-2)
   expect_true(get.mse("asgd", "rmsprop") < 1e-2)
   expect_true(get.mse("ai-sgd", "one-dim") < 1e-2)
-  #expect_true(get.mse("ai-sgd", "one-dim-eigen") < 1e-2)
   expect_true(get.mse("ai-sgd", "d-dim") < 1e-2)
   expect_true(get.mse("ai-sgd", "adagrad") < 1e-2)
   expect_true(get.mse("ai-sgd", "rmsprop") < 1e-2)
   expect_true(get.mse("momentum", "one-dim") < 1e-2)
-  #expect_true(get.mse("momentum", "one-dim-eigen") < 1e-2)
   expect_true(get.mse("momentum", "d-dim") < 1e-2)
   expect_true(get.mse("momentum", "adagrad") < 1e-2)
   expect_true(get.mse("momentum", "rmsprop") < 1e-2)
   expect_true(get.mse("nesterov", "one-dim") < 1e-2)
-  #expect_true(get.mse("nesterov", "one-dim-eigen") < 1e-2)
   expect_true(get.mse("nesterov", "d-dim") < 1e-2)
   expect_true(get.mse("nesterov", "adagrad") < 1e-2)
   expect_true(get.mse("nesterov", "rmsprop") < 1e-2)
